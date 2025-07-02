11930e5c9f0a796a69b4af8a5c3de41a7a280f77
jtb <bloo1987@proton.me>
2025-07-01 18:36:07 -0700

    first

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+use anyhow::{Context, Error, Result, anyhow};
+use bitfield::{Bit, BitRange};
+use bytes::Bytes;
+
+// Since the protocol is binary transparent, STX and carriage
+// return characters are not allowed in the data field. Need escape character plus
+// a way to signal \r and 0x02.
+const ESCAPE_CHAR: u8 = 0x07;
+const HEX_02: u8 = 0x30; // ASCII '0'
+const HEX_0D: u8 = 0x31; // ASCII '1'
+const HEX_07: u8 = 0x32; // ASCII '2'
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum ResponseCode {
+    /// Command understood and executed. 0x01
+    Ok,
+    /// Illegal command (CMD code not valid). 0x02
+    ErrInvalidCmd,
+    /// Syntax error. (too many bytes in data field, not enough bytes, etc). 0x03
+    ErrSyntax,
+    /// Data range error. 0x04
+    ErrRange,
+    /// Inhibited. 0x05
+    ErrInhibited,
+    /// Obsolete command. No action taken, but not really an error. 0x07
+    ErrObsolete,
+    /// Reserved for future protocol stack use. 0x07
+    Reserved,
+}
+impl TryFrom<u8> for ResponseCode {
+    type Error = Error;
+
+    fn try_from(code: u8) -> Result<Self, Self::Error> {
+        let res = match code {
+            0x01 => ResponseCode::Ok,
+            0x02 => ResponseCode::ErrInvalidCmd,
+            0x03 => ResponseCode::ErrSyntax,
+            0x04 => ResponseCode::ErrRange,
+            0x05 => ResponseCode::ErrInhibited,
+            0x06 => ResponseCode::ErrObsolete,
+            0x07 => ResponseCode::Reserved,
+            other => return Err(anyhow!("{} is not a valid response code.", other)),
+        };
+        Ok(res)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum CommandCode {
+    /// Reserved for future protocol stack use. 0x01, 0x02
+    Reserved,
+    /// Product ID, returned as decimal string. 0x03
+    ProdId,
+    /// Request slave to return software version string. 0x04
+    SwVersion,
+    /// Request slave to reset. 0x05
+    Reset,
+    /// Request slave to clear RSPF bit/flag. 0x06
+    AckPf,
+    /// Request slave to return protocol stack version as decimal string. 0x07
+    ProcotolVer,
+    /// Available for application use.
+    App(u8),
+}
+impl TryFrom<u8> for CommandCode {
+    type Error = Error;
+
+    fn try_from(code: u8) -> Result<Self, Self::Error> {
+        let res = match code {
+            0x01 | 0x02 => CommandCode::Reserved,
+            0x03 => CommandCode::ProdId,
+            0x04 => CommandCode::SwVersion,
+            0x05 => CommandCode::Reset,
+            0x06 => CommandCode::AckPf,
+            0x07 => CommandCode::ProcotolVer,
+            c @ 0x08..=0x0F => CommandCode::App(c),
+            other => return Err(anyhow!("{} is not a valid command code.", other)),
+        };
+        Ok(res)
+    }
+}
+pub(crate) struct CommandResponse(u8);
+impl CommandResponse {
+    /// CMD = Command. These are the commands the master can issue to the slave. All
+    /// Sycon products must respond to commands in the range of 1-7. CMDS 1-7 are
+    /// handled in the protocol, at the protocol layer. Applications are not to use
+    /// commands 1-7 except to implement the protocol specification. CMD codes 8-15
+    /// are set aside for product application specific use.
+    pub(crate) fn cmd(&self) -> Result<CommandCode> {
+        let code: u8 = self.0.bit_range(7, 4);
+        code.try_into()
+    }
+    /// Response Flag? Going from master to slave, RSPF
+    /// bit is zero. From slave to master if this bit is 1, then the slave
+    /// has been reset since the last ack power fail flag was received.
+    pub(crate) fn rspf(&self) -> bool {
+        self.0.bit(3)
+    }
+    /// Once the packet is sent to the slave, the slave receives and acknowledges the
+    /// packet, and sends it’s response. In the CMD_RSP byte, the CMD bits are
+    /// unchanged from the master, but the RSP bits are filled in according to the status
+    /// of the slave (from table)
+    pub(crate) fn rsp(&self) -> Result<ResponseCode> {
+        let code: u8 = self.0.bit_range(2, 0);
+        code.try_into()
+    }
+}
+pub struct SmdpPacket {
+    /// Start of text character (hex 02). Multiple STX characters in a row are allowed.
+    /// Similarly, any data between STX characters is ignored. A single STX character
+    /// syncs the receiver up to receive a new message, purging any data collected since
+    /// the last STX char or carriage return received.
+    stx: u8,
+    /// Address field. This is a one byte field. Valid ranges of a valid address are 10 hex
+    /// to FE hex (16 to 254 decimal). Addresses less than 10 hex are not allowed as they
+    /// may be mistaken for framing characters. An address of FF hex is reserved as it is
+    /// used as an extention, to indicate another byte of address information follows, for
+    /// products that have an address range higher than an address of FE hex.
+    addr: u8,
+    /// Command/Response field. When packet is going from master to slave, RSPF
+    /// bit is zero, and the RSP field (3 bits) is zero. When packet is going from slave to
+    /// master, CMD bits are the same as in the message that was sent, but the RSP field
+    /// will be non-zero (indicating actual unit response status). This allows the direction
+    /// to be positively indicated, as well as the command preserved on the return reply.
+    cmd_rsp: CommandResponse,
+    data: Vec<u8>,
+    checksum_1: u8,
+    checksum_2: u8,
+}
