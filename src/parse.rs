e07f1befbf5641a009560af7593fd36cd2943915
jtb <bloo1987@proton.me>
2025-07-02 20:47:04 -0700

    adding guts now

--- src/parse.rs
+++ src/parse.rs
@@ -6,14 +6,14 @@ use std::{
     time::Duration,
 };
 
-/// Required methods for a Framer
+/// Required methods for a Framer. Primarily used for testing.
 trait Framer {
     /// Push bytes into Framer buffer.
     fn push_bytes(&mut self, data: &[u8]) -> FramerStatus;
     /// Called by Reader to signal finished status.
     fn finalize(&mut self) -> FramerStatus;
     /// Yields next valid frame if found.
-    fn next_packet(&mut self) -> Option<Bytes>;
+    fn next_packet(&mut self) -> Result<Bytes>;
 }
 
 /// Feedback to Reader on the state of the
@@ -34,6 +34,19 @@ struct SmdpFramer {
     // Max size of the buffer/packet (including STX/ETX)
     max_size: usize,
 }
+impl Framer for SmdpFramer {
+    fn push_bytes(&mut self, data: &[u8]) -> FramerStatus {
+        todo!()
+    }
+
+    fn finalize(&mut self) -> FramerStatus {
+        todo!()
+    }
+
+    fn next_packet(&mut self) -> Result<Bytes> {
+        todo!()
+    }
+}
 /// Manages reading from a buffer, retries, timeouts, etc.
 struct SmdpReader<T: Read + Write, F: Framer> {
     io_handle: T,
@@ -44,17 +57,48 @@ struct SmdpReader<T: Read + Write, F: Framer> {
     read_timeout: Duration,
     // Size of read chunk when iterating over input buffer.
     read_chunk_size: usize,
+    // Read buffer
+    read_buf: Vec<u8>,
 }
-// Handles packet validation after receiving a candidate packet from the framer.
-// Returns valid, deserialized SmdpPacket.
-struct SmdpParser {
-    framed_bytes: Bytes,
+impl<T, F> SmdpReader<T, F>
+where
+    T: Read + Write,
+    F: Framer,
+{
+    fn new(
+        io_handle: T,
+        framer: F,
+        read_timeout: usize,
+        read_chunk_size: usize,
+        max_frame_size: usize,
+    ) -> Self {
+        todo!()
+    }
+    // Attempts to read and frame bytes after a request for one
+    // candidate SMDP packet.
+    fn poll_once(&mut self) -> Result<Bytes> {
+        todo!()
+    }
 }
 
 /// Packages all the individual components to go from the wire to serialized SmdpPacket.
-struct SmpdProtocol {}
-
-/// Function that parses a frame
-fn parse_frame(frame: Bytes) -> Option<SmdpPacket> {
-    todo!()
+pub struct SmpdProtocol<T: Read + Write> {
+    reader: SmdpReader<T, SmdpFramer>,
+}
+impl<T> SmpdProtocol<T>
+where
+    T: Read + Write,
+{
+    fn parse_frame(&mut self, frame: Bytes) -> Result<SmdpPacket> {
+        // Verify Address is in-range
+        // Verify fields of CMD_RSP byte are valid
+        // Unescape Data field
+        // Verify checksum
+        // Deserialize into packet struct
+        todo!()
+    }
+    /// Tries to read one SMDP packet from the wire after a request.
+    pub fn poll_once(&mut self) -> Result<SmdpPacket> {
+        todo!()
+    }
 }
