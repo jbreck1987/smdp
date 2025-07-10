2006996334ad71c6160776c9643624e187679ad2
jtb <bloo1987@proton.me>
2025-07-09 17:11:05 -0700

    finished re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,19 @@ mod error;
 pub mod format;
 pub mod parse;
 
+// Errors
 pub use error::Error;
 pub use error::ErrorKind;
 
+// Traits
 pub use format::DeserializePacket;
 pub use format::PacketFormat;
 pub use format::SerizalizePacket;
+pub use parse::Framer;
+
+// Structs
 pub use format::SmdpPacketV1;
 pub use format::SmdpPacketV2;
-pub use parse::Framer;
 pub use parse::SmdpPacketHandler;
 
 #[cfg(test)]
